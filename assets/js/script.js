a2f05f269b8cd7d726c1dbd5103015c06e7a4895
urbanIntrovert <trefoil72@gmail.com>
2025-08-08 04:51:39 +0100

    feat: style modal and add open and close functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,4 +22,71 @@ document.addEventListener('DOMContentLoaded', () => {
         const label = currentTheme === 'dark' ? 'Switch to light mode' : 'Switch to dark mode';
         toggleBtn.setAttribute('aria-label', label);
     }
+
+    const modalButtons = document.querySelectorAll('[data-modal]');
+    const modals = document.querySelectorAll('.modal');
+    const body = document.body;
+
+    modalButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const modalId = button.getAttribute('data-modal');
+            const modal = document.getElementById(modalId);
+            openModal(modal);
+        });
+    });
+
+    modals.forEach(modal => {
+        modal.addEventListener('click', e => {
+            const modalContent = modal.querySelector('.modal-content');
+            if (!modalContent.contains(e.target)) {
+                closeModal(modal);
+            }
+        });
+    });
+
+    function openModal(modal) {
+        if (!modal) return;
+        modal.hidden = false;
+        modal.setAttribute('aria-hidden', 'false');
+        body.classList.add('modal-open');
+        // modal.querySelector('.close-modal').focus();
+
+        // const closeBtn = modal.querySelector('.close-modal');
+
+        // closeBtn.addEventListener('click', () => closeModal(modal));
+
+        const closeBtn = modal.querySelector('.close-modal');
+
+        requestAnimationFrame(() => {
+            if (closeBtn) {
+                closeBtn.focus();
+                closeBtn.onclick = () => closeModal(modal);
+            }
+        });
+
+        // document.addEventListener('keydown', escHandler);
+
+        // function escHandler(e) {
+        //     if (e.key === 'Escape') {
+        //         closeModal(modal);
+
+        //         document.removeEventListener('keydown', escHandler);
+        //     }
+        // }
+        const escHandler = e => {
+            if (e.key === 'Escape') {
+                closeModal(modal);
+
+                document.removeEventListener('keydown', escHandler);
+            }
+        };
+
+        document.addEventListener('keydown', escHandler);
+    }
+
+    function closeModal(modal) {
+        modal.hidden = true;
+        modal.setAttribute('aria-hidden', 'true');
+        body.classList.remove('modal-open');
+    }
 });
